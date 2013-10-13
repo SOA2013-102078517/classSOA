dbd7755092b93443761411f800a13878c242dcfb
saasbook <saasbook@saasbook.(none)>
2013-10-13 09:43:28 -0700

    ‘ABC’

--- classSOA/myrottenpotatoes/config/routes.rb
+++ classSOA/myrottenpotatoes/config/routes.rb
@@ -1,4 +1,4 @@
-Myrottenpotatoes::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -56,9 +56,9 @@ Myrottenpotatoes::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-# Myrottenpotatoes::Application.routes.draw do
+Myrottenpotatoes::Application.routes.draw do
   resources :movies
   root :to => redirect('/movies')
-# end
+ end
 
-end
+
